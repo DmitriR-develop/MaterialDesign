dae60f15d861345b49a0e496e70e976ad3582ae7
dmitri <dmitrivitr@gmail.com>
2021-05-07 19:16:27 +0300

    less5

--- app/src/main/java/com/example/materialdesign/ui/viewPager/TodayFragment.kt
+++ app/src/main/java/com/example/materialdesign/ui/viewPager/TodayFragment.kt
@@ -9,10 +9,17 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.transition.ChangeBounds
+import androidx.transition.ChangeImageTransform
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import coil.api.load
 import com.example.materialdesign.R
+import com.example.materialdesign.ui.MainActivity
 import com.example.materialdesign.ui.picture.PictureOfTheDayData
 import com.example.materialdesign.ui.picture.PictureOfTheDayViewModel
+import kotlinx.android.synthetic.main.fragment_today.*
+import kotlinx.android.synthetic.main.main_activity.*
 import java.util.*
 
 class TodayFragment : Fragment() {
@@ -20,6 +27,7 @@ class TodayFragment : Fragment() {
     private val viewModel: PictureOfTheDayViewModel by lazy {
         ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
     }
+    private var isExpanded = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +43,30 @@ class TodayFragment : Fragment() {
         val date = "${currentDate.year}-${currentDate.month}-${currentDate.day}"
         viewModel.getData(date)
             .observe(viewLifecycleOwner, Observer<PictureOfTheDayData> { renderData(it) })
+
+        MainActivity().setContentView(R.layout.fragment_today)
+        image_view_today.setOnClickListener {
+            isExpanded = !isExpanded
+            TransitionManager.beginDelayedTransition(
+                container, TransitionSet()
+                    .addTransition(ChangeBounds())
+                    .addTransition(ChangeImageTransform())
+            )
+            val params: ViewGroup.LayoutParams = image_view_today.layoutParams
+            params.height =
+                if (isExpanded) {
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                } else {
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                }
+            image_view_today.layoutParams = params
+            image_view_today.scaleType =
+                if (isExpanded) {
+                    ImageView.ScaleType.CENTER_CROP
+                } else {
+                    ImageView.ScaleType.FIT_CENTER
+                }
+        }
     }
 
     private fun renderData(data: PictureOfTheDayData) {
--- app/src/main/java/com/example/materialdesign/ui/viewPager/YesterdayFragment.kt
+++ app/src/main/java/com/example/materialdesign/ui/viewPager/YesterdayFragment.kt
@@ -9,16 +9,25 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.transition.ChangeBounds
+import androidx.transition.ChangeImageTransform
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import coil.api.load
 import com.example.materialdesign.R
+import com.example.materialdesign.ui.MainActivity
 import com.example.materialdesign.ui.picture.PictureOfTheDayData
 import com.example.materialdesign.ui.picture.PictureOfTheDayViewModel
+import kotlinx.android.synthetic.main.fragment_today.*
+import kotlinx.android.synthetic.main.fragment_yesterday.*
+import kotlinx.android.synthetic.main.main_activity.*
 
 class YesterdayFragment : Fragment() {
 
     private val viewModel: PictureOfTheDayViewModel by lazy {
         ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
     }
+    private var isExpanded = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,6 +43,30 @@ class YesterdayFragment : Fragment() {
         val date = "${currentDay.year}-${currentDay.month}-${currentDay.day - 1}"
         viewModel.getData(date)
             .observe(viewLifecycleOwner, Observer<PictureOfTheDayData> { renderData(it) })
+
+        MainActivity().setContentView(R.layout.fragment_yesterday)
+        image_view_yesterday.setOnClickListener {
+            isExpanded = !isExpanded
+            TransitionManager.beginDelayedTransition(
+                container, TransitionSet()
+                    .addTransition(ChangeBounds())
+                    .addTransition(ChangeImageTransform())
+            )
+            val params: ViewGroup.LayoutParams = image_view_today.layoutParams
+            params.height =
+                if (isExpanded) {
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                } else {
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                }
+            image_view_today.layoutParams = params
+            image_view_today.scaleType =
+                if (isExpanded) {
+                    ImageView.ScaleType.CENTER_CROP
+                } else {
+                    ImageView.ScaleType.FIT_CENTER
+                }
+        }
     }
 
     private fun renderData(data: PictureOfTheDayData) {
